1467e739088388f397c312025f148685ed531838
Johanna Palo <joa.palobael@gmail.com>
2023-01-19 08:46:19 -0300

    Navbar responsive finished

---
+++ 04-navbar/js/navbar.js
@@ -0,0 +1,10 @@
+//Elegir elementos del DOM
+const menuIcon = document.querySelector('.menu-icon');
+const links = document.querySelector('.links');
+
+//Escuchar el evento click del botón
+
+menuIcon.addEventListener('click', function(){
+    //Condicional: Si tiene la clase show-links, se la saco. Si no la tiene, la agrego.
+    links.classList.toggle('show-links');
+})
